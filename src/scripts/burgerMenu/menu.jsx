1676f2e80775bdf0d04d16b3d81f33eb155e57cf
Dimon <gorokhov.diman@mail.ru>
2023-05-16 21:22:31 +0300

    init commit

--- src/scripts/burgerMenu/menu.jsx
+++ src/scripts/burgerMenu/menu.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import s from './menu.module.css';
+import { NavBar } from '../navbar/navbar';
 
-
-const Menu = ({item, active, setActive}) => {
+const Menu = ({active, setActive}) => {
+    
     return (
         <div className={`${s.menu} ${active ? s.active : ''}`} onClick={() => setActive(false)}>
             <div className={s.nav__menu}>
                 <ul className={s.menu__list}>
-                    { 
-                    item.map((item) => 
-                        <li className="menu__item" key={item.id}>
-                            <a href={item.href}>{item.value}</a>
-                        </li>
-                    )}
+                <NavBar/>
                 </ul>
             </div>
         </div>
@@ -20,5 +16,3 @@ const Menu = ({item, active, setActive}) => {
 }
 
 export default Menu;
-
-
